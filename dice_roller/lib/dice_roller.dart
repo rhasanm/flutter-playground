9b1c0011eb15a7688d6d53e1838f6e4259cff1fa
rhasanm <hasanrakibul.masum@gmail.com>
2023-08-05 19:56:51 +0600

    DiceRoller App Finalized

--- dice_roller/lib/dice_roller.dart
+++ dice_roller/lib/dice_roller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomize = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -10,14 +13,36 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceFace = 2;
+  void diceRolled() {
+    setState(() {
+      currentDiceFace = randomize.nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(context) {
-    return const Text(
-      'Dice Roller',
-      style: TextStyle(
-        fontSize: 28,
-        color: Colors.white,
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceFace.png',
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: diceRolled,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
     );
   }
 }
